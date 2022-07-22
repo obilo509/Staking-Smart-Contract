573a80451a992cef4382b0bec76a0bfe7c0b4f06
Obilo Christian <67980059+obilo509@users.noreply.github.com>
2022-07-22 11:24:18 -0700

    Update Stake.sol

--- Stake.sol
+++ Stake.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "hardhat/console.sol";
-import "./ExampleExternalContract.sol";
-
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
